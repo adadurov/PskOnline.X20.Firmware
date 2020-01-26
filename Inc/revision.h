91eec87ee358977792964a6026a31eaad549804b
Alexey Adadurov <alexei.adadurov@mail.ru>
2020-01-27 01:03:21 +0300

    remove 'alpha' from 0.2.0 release branch

--- Inc/revision.h
+++ Inc/revision.h
@@ -1,5 +1,5 @@
 
 #define BUILD_DATE (__DATE__ " " __TIME__)
 
-#define REVISION_INFO "0.2.0-alpha"
+#define REVISION_INFO "0.2.0"
 //#define REVISION_INFO "0.1.0-alpha-a8f44830c601c15961c19599cc0a3d8c0bbfc8e8"
