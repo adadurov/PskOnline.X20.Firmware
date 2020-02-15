358dbea659c955b8cc8b7148c12c1ccc33e8a13a
Alexey Adadurov <alexei.adadurov@mail.ru>
2020-02-15 17:05:48 +0300

    update firmware revision number to 0.3.0 fo release

--- Inc/revision.h
+++ Inc/revision.h
@@ -2,5 +2,5 @@
 #define BUILD_DATE (__DATE__ " " __TIME__)
 
 // Due to bug AB#295, this string must be at most 30 Latin characters long.
-#define REVISION_INFO "0.3.0-alpha"
+#define REVISION_INFO "0.3.0"
 //#define REVISION_INFO "0.1.0-alpha-a8f44830c601c15961c19599cc0a3d8c0bbfc8e8"
